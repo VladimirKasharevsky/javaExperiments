598e7eb74475bc3eaf79e15e4e7adab487250867
Vladimir Kasharevskiy <kashar015@rambler.ru>
2021-07-16 23:11:46 +0400

    Добавлена реализация двоичного дерева

--- src/Tree.java
+++ src/Tree.java
@@ -2,5 +2,33 @@ public class Tree {
     public static void main(String[] args) {
 
 
+        Node root =
+                new Node(20,
+                        new Node(7,
+                                new Node(4, null, new Node(6)), new Node(9)),
+                        new Node(35,
+                                new Node(31, new Node(28), null),
+                                new Node(40, new Node(38), new Node(52))));
+
+//        System.out.println("Сумма дерева: " + root.sum());
     }
+
+
+
+ static class Node {
+    int value;
+    Node left;
+    Node right;
+
+    public Node(int value, Node left, Node right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+
+     public Node(int value) {
+         this.value = value;
+     }
+
+//     https://github.com/Arhiser/java_tutorials/blob/master/src/ru/arhiser/tree1/tree.java
 }
