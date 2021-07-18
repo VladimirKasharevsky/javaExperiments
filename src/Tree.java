ee5206109461b6e2c506aefea76163a905420710
VladimirKasharevsky <kashar015@rambler.ru>
2021-07-18 17:44:45 +0300

    Добалены алгоритмы поиска суммы узлов дерева в ширину и глубину

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,7 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Tree {
     public static void main(String[] args) {
 
@@ -10,25 +14,88 @@ public class Tree {
                                 new Node(31, new Node(28), null),
                                 new Node(40, new Node(38), new Node(52))));
 
-//        System.out.println("Сумма дерева: " + root.sum());
+        System.out.println("Сумма дерева: " + root.sum());
+        System.out.println("Сумма дерева: " + sumDeep(root));
     }
 
+//Объект дерево
+    static class Node {
+        int value;
+        Node left;
+        Node right;
 
+        public Node(int value, Node left, Node right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
 
- static class Node {
-    int value;
-    Node left;
-    Node right;
-
-    public Node(int value, Node left, Node right) {
-        this.value = value;
-        this.left = left;
-        this.right = right;
-    }
-
-     public Node(int value) {
-         this.value = value;
-     }
+        public Node(int value) {
+            this.value = value;
+        }
 
 //     https://github.com/Arhiser/java_tutorials/blob/master/src/ru/arhiser/tree1/tree.java
+
+    //Сумма всех элементов
+        public int sum() {
+            int summ = value;
+
+            if (left != null) {
+                summ += left.sum();
+            }
+
+            if (right != null) {
+                summ += right.sum();
+            }
+            return summ;
+        }
+    }
+
+    public static int sumDeep(Node root) {
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+
+        int summ = 0;
+
+        while (!stack.isEmpty()) {
+            Node node = stack.pop();
+
+            System.out.println(node.value);
+
+            summ = summ + node.value;
+
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return summ;
+    }
+
+    public static int sumWide(Node root) {
+        Queue<Node> stack = new PriorityQueue<>();
+        stack.add(root);
+
+        int summ = 0;
+
+        while (!stack.isEmpty()) {
+            Node node = stack.poll();
+
+            System.out.println(node.value);
+
+            summ = summ + node.value;
+
+            if (node.right != null) {
+                stack.add(node.right);
+            }
+
+            if (node.left != null) {
+                stack.add(node.left);
+            }
+        }
+        return summ;
+    }
 }
